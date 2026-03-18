1c170069c65f3b2c4e60be9d96fddcafa29e898d
SandraK0z <r1047763@student.thomasmore.be>
2026-03-18 16:42:29 +0100

    feat: (DetailPagina)Interactieve aantal-artikelenknop

--- Screens/ProductDetails.js
+++ Screens/ProductDetails.js
@@ -1,16 +1,42 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 const ProductDetails = ({ route }) => { 
   const { titel, prijs, foto } = route.params;
 
+  const [quantity, setQuantity] = useState(1);
+  const incrementQuantity = () => setQuantity(quantity + 1);
+  const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
   return (
     <ScrollView style={styles.container}>
       <Image source={{ uri: foto }} style={styles.detailImage} resizeMode="contain" />
       <View style={styles.content}>
         <Text style={styles.header}>{titel}</Text>
         <Text style={styles.price}>€ {prijs}</Text>
-        <Text style={styles.description}>Hier komt de productbeschrijving...</Text>
+        <Text style={styles.description}> productbeschrijving...</Text>
       </View>
+      <View style={styles.content}>
+
+          <View style={styles.quantityContainer}>
+        <TouchableOpacity onPress={decrementQuantity} style={styles.quantityButton}>
+          <Text style={styles.quantityButtonText}>-</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.quantityText}>{quantity}</Text>
+
+          <TouchableOpacity onPress={incrementQuantity} style={styles.quantityButton}>
+            <Text style={styles.quantityButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.totalPrice}>Totaal: € {prijs * quantity}</Text>
+        </View>
+      
+
     </ScrollView>
   );
 };
